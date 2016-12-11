0634f0dca6a684f2a575843d233bb7cf029c3699
Laser Seymour Kaplan <laserkaplan@gmail.com>
2016-12-11 16:15:34 +0100

    Add TAM and soft drop

--- Root/dijetISR_MiniTree.cxx
+++ Root/dijetISR_MiniTree.cxx
@@ -13,6 +13,10 @@ void dijetISR_MiniTree::AddEventUser(const std::string) {
 void dijetISR_MiniTree::AddFatJetsUser(const std::string, const std::string, const std::string) {
     m_tree->Branch("fatjet_pt_ungroomed", &m_fatjet_pt_ungroomed);
     m_tree->Branch("fatjet_tau21_wta_ungroomed", &m_fatjet_tau21_wta_ungroomed);
+    m_tree->Branch("fatjet_tam", &m_fatjet_tam);
+    m_tree->Branch("fatjet_sd_m", &m_fatjet_sd_m);
+    m_tree->Branch("fatjet_sd_pt", &m_fatjet_sd_pt);
+    m_tree->Branch("fatjet_sd_tau21_wta", &m_fatjet_sd_tau21_wta);
 }
 
 void dijetISR_MiniTree::FillEventUser(const xAOD::EventInfo *eventInfo) {
@@ -20,6 +24,7 @@ void dijetISR_MiniTree::FillEventUser(const xAOD::EventInfo *eventInfo) {
 }
 
 void dijetISR_MiniTree::FillFatJetsUser(const xAOD::Jet *fatjet, const std::string, const std::string) {
+    // ungroomed pt and tau21
     const xAOD::Jet *parent = 0;
     auto el = fatjet->auxdata<ElementLink<xAOD::JetContainer> >("Parent");
     if (el.isValid()) parent = *el;
@@ -36,6 +41,72 @@ void dijetISR_MiniTree::FillFatJetsUser(const xAOD::Jet *fatjet, const std::stri
         m_fatjet_pt_ungroomed.push_back(-999.);
         m_fatjet_tau21_wta_ungroomed.push_back(-999.);
     }
+
+    // track assisted mass
+    static SG::AuxElement::ConstAccessor<float> acc_TrackSumPt("TrackSumPt");
+    static SG::AuxElement::ConstAccessor<float> acc_TrackSumMass("TrackSumMass");
+    if (acc_TrackSumPt.isAvailable(*fatjet) && acc_TrackSumMass.isAvailable(*fatjet)) m_fatjet_tam.push_back(acc_TrackSumMass(*fatjet) * (fatjet->pt() / acc_TrackSumPt(*fatjet)) / 1000.);
+    else m_fatjet_tam.push_back(-999.);
+
+    // soft drop mass
+    static fastjet::contrib::SoftDrop tool_SoftDrop(0.0, 0.1);
+    static fastjet::contrib::Nsubjettiness nSub1_beta1(1, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(1.0));
+    static fastjet::contrib::Nsubjettiness nSub2_beta1(2, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(1.0));
+
+    float sd_m = -999.;
+    float sd_pt = -999.;
+    float sd_tau21_wta = -999.;
+    if (parent) {
+        fastjet::PseudoJet jet_SoftDrop;
+        
+        // if pseudo jet still available, use that
+        const fastjet::PseudoJet *pdj = parent->getPseudoJet();
+        if (pdj) {
+            jet_SoftDrop = tool_SoftDrop(*pdj);
+            sd_m = jet_SoftDrop.m() / 1000.;
+            sd_pt = jet_SoftDrop.pt() / 1000.;
+            sd_tau21_wta = nSub2_beta1(jet_SoftDrop) / nSub1_beta1(jet_SoftDrop);
+        }
+        // otherwise, reconstruct it
+        else {
+            // clusters from jet (if we don't have these, we are SOL anyways)
+            xAOD::JetConstituentVector clusters = parent->getConstituents();
+            
+            if (clusters.size()) {
+                // make pseudo jets of the clusters
+                std::vector<fastjet::PseudoJet> jet_inputs;
+                TLorentzVector temp_p4;
+                for (auto c : clusters) {
+                    if (c->e()<0) continue;
+                    temp_p4.SetPtEtaPhiM(c->pt(), c->eta(), c->phi(), c->m());
+                    jet_inputs.push_back(fastjet::PseudoJet(temp_p4.Px(),temp_p4.Py(),temp_p4.Pz(),temp_p4.E()));
+                }
+
+                // anti-kt it up
+                fastjet::JetDefinition jet_def = fastjet::JetDefinition(fastjet::antikt_algorithm, 1.0, fastjet::E_scheme, fastjet::Best);
+                fastjet::ClusterSequence clust_seq = fastjet::ClusterSequence(jet_inputs, jet_def);
+                std::vector<fastjet::PseudoJet> pjets =  fastjet::sorted_by_pt(clust_seq.inclusive_jets(0.0));
+
+                // find the one that is our parent jet (pt matching)
+                fastjet::PseudoJet jet_Ungroomed;
+                float diff = 999999.;
+                for (auto jet : pjets) {
+                    if (fabs(jet.pt() - fatjet->pt()) < diff) {
+                        jet_Ungroomed = jet;
+                        diff = fabs(jet.pt() - fatjet->pt());
+                    }
+                }
+
+                jet_SoftDrop = tool_SoftDrop(jet_Ungroomed);
+                sd_m = jet_SoftDrop.m() / 1000.;
+                sd_pt = jet_SoftDrop.pt() / 1000.;
+                sd_tau21_wta = nSub2_beta1(jet_SoftDrop) / nSub1_beta1(jet_SoftDrop);
+            }
+        }
+    }
+    m_fatjet_sd_m.push_back(sd_m);
+    m_fatjet_sd_pt.push_back(sd_pt);
+    m_fatjet_sd_tau21_wta.push_back(sd_tau21_wta);
 }
 
 void dijetISR_MiniTree::ClearEventUser() {
@@ -45,4 +116,8 @@ void dijetISR_MiniTree::ClearEventUser() {
 void dijetISR_MiniTree::ClearFatJetsUser(const std::string, const std::string) {
     m_fatjet_pt_ungroomed.clear();
     m_fatjet_tau21_wta_ungroomed.clear();
+    m_fatjet_tam.clear();
+    m_fatjet_sd_m.clear();
+    m_fatjet_sd_pt.clear();
+    m_fatjet_sd_tau21_wta.clear();
 }
--- dijetISR/dijetISR_MiniTree.h
+++ dijetISR/dijetISR_MiniTree.h
@@ -4,12 +4,19 @@
 #include "xAODAnaHelpers/HelpTreeBase.h"
 
 #include "xAODEventInfo/EventInfo.h"
+#include "fastjet/contrib/SoftDrop.hh"
+#include "fastjet/ClusterSequence.hh"
+#include "fastjet/contrib/Nsubjettiness.hh"
 
 class dijetISR_MiniTree : public HelpTreeBase {
     private:
         float m_weight;
         std::vector<float> m_fatjet_pt_ungroomed;
         std::vector<float> m_fatjet_tau21_wta_ungroomed;
+        std::vector<float> m_fatjet_tam;
+        std::vector<float> m_fatjet_sd_m;
+        std::vector<float> m_fatjet_sd_pt;
+        std::vector<float> m_fatjet_sd_tau21_wta;
 
     public:
         dijetISR_MiniTree(xAOD::TEvent *event, TTree *tree, TFile *file);
